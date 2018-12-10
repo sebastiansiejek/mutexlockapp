5b638f69be92e49883b5b2aec0dc65ea52457bab
Sebastian Siejek <siejeksebastian@gmail.com>
2018-12-10 19:59:52 +0100

    Initial Commit

---
+++ MutexLockApp/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 10/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
