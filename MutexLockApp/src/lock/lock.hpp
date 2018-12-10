ef13a91a32e0d01d6d25e7b9dff569acb16dfa92
Sebastian Siejek <siejeksebastian@gmail.com>
2018-12-10 20:10:03 +0100

    Add lock files

---
+++ MutexLockApp/src/lock/lock.hpp
@@ -0,0 +1,14 @@
+//
+//  lock.hpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 10/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#ifndef lock_hpp
+#define lock_hpp
+
+#include <stdio.h>
+
+#endif /* lock_hpp */
