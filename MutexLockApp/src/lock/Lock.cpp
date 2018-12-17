0e138512b8b98c36ed0ce854a22f59b87d2b4017
Sebastian Siejek <siejeksebastian@gmail.com>
2018-12-17 19:59:15 +0100

    new Lock

---
+++ MutexLockApp/src/lock/Lock.cpp
@@ -0,0 +1,14 @@
+//
+//  lock.cpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 10/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#include "Lock.hpp"
+
+Lock::Lock(std::string name)
+{
+    this->name = name;
+}
