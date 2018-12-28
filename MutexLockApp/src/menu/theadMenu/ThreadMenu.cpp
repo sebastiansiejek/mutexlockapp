fdf8fa951660e8ff3a1e2477786abe99bbbd0983
Sebastian Siejek <siejeksebastian@gmail.com>
2018-12-28 15:30:40 +0100

    add ThreadMenu class

---
+++ MutexLockApp/src/menu/theadMenu/ThreadMenu.cpp
@@ -0,0 +1,14 @@
+//
+//  ThreadMenu.cpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 28/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#include "ThreadMenu.hpp"
+
+class ThreadMenu
+{
+    
+}
---
+++ MutexLockApp/src/menu/theadMenu/ThreadMenu.hpp
@@ -0,0 +1,14 @@
+//
+//  ThreadMenu.hpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 28/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#ifndef ThreadMenu_hpp
+#define ThreadMenu_hpp
+
+#include <stdio.h>
+
+#endif /* ThreadMenu_hpp */
