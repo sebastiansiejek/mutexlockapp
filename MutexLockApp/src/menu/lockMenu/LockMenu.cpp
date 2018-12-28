51876872fa84b176c694b80acca715c0e242497e
Sebastian Siejek <siejeksebastian@gmail.com>
2018-12-28 11:53:48 +0100

    add LockMenu Class

---
+++ MutexLockApp/src/menu/lockMenu/LockMenu.cpp
@@ -0,0 +1,9 @@
+//
+//  LockMenu.cpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 28/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#include "LockMenu.hpp"
---
+++ MutexLockApp/src/menu/lockMenu/LockMenu.hpp
@@ -0,0 +1,20 @@
+//
+//  LockMenu.hpp
+//  MutexLockApp
+//
+//  Created by Sebastian Siejek on 28/12/2018.
+//  Copyright © 2018 Sebastian Siejek. All rights reserved.
+//
+
+#ifndef LockMenu_hpp
+#define LockMenu_hpp
+
+#include <stdio.h>
+#include "../Menu.hpp"
+
+class LockMenu : public Menu
+{
+    
+};
+
+#endif /* LockMenu_hpp */
